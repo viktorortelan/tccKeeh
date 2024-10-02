e35077ec751d1ebf9a6e3d7f5e48f61d2d7bf591
SandesKeh <kevillynsandes07@gmail.com>
2024-10-01 23:22:02 -0300

    cliente

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -66,6 +66,40 @@ export default function Home() {
                     </svg>
                 </div>
             </div>
+
+            <div className="card">
+                <div className="cards">
+                    <div className="cabecalho">
+                        <h1> 1. Informações Pessoais</h1>
+                        
+                        <div className="i">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z"/>
+                            </svg>
+                         </div>  
+                        
+                    </div>  
+                    <div className="contener">
+
+                        <div className="inpu">
+                            <h1>Nome </h1>
+
+                            <input type="text" />
+
+                            <h1> Grupo </h1>
+
+                            <input type="text" />
+
+                            <h1> Data de Nascimento</h1>
+
+                            <input type="text" />
+
+                            <h1> </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
         </div>
     )
 }
