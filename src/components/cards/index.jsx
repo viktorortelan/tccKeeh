2947b9cc9f47afcf5e305e5f5e0ca5a543393c54
SandesKeh <kevillynsandes07@gmail.com>
2024-10-03 22:36:06 -0300

    informaçoes responsavel. part Cliente

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react";
 import './index.scss';
 
-export default function Card() {
+export default function Card(props) {
     const [exibir, setExibir] = useState(true);
 
     const mostrar = () => {
         setExibir(!exibir);
     };
+
+    const [exibi, setExibi] = useState(true);
+
+    const colocar = () => {
+        setExibi(!exibi);
+    };
+
+    const [visualizar, setVisualizar] = useState(true);
+
+    const endereco = () => {
+        setVisualizar (!visualizar);
+    }
+
+    const [coloca, setColoca] = useState(true);
+
+    const enderec = () => {
+        setColoca(!coloca);
+    }
+
+
 
     return (
         <div className="cards">
@@ -20,14 +40,16 @@ export default function Card() {
              </button>  
             
         </div>  
-        <div className={`contener ${exibir ? "show" : "hide"}`}>  {/* sei que vc nn sabe o que é isso, isso é basicamente um if else, ele valida no scss se for verdadeiro ou falso. */}
-       
+
+     
+        
+        <div className={`contener ${exibir ? "show" : "hide"}`}>  
            
 
                 <div className="cont1">
                     <div className="inpute">
                         <h1>Nome: </h1>
-                        <input type="text" />
+                        <input type="text" placeholder="Digite seu nome"/>
                     </div>
                 </div>    
                 
@@ -35,7 +57,7 @@ export default function Card() {
                     <div className="inpute">
                         <h1> Grupo: </h1>
 
-                        <input type="text" />
+                        <input type="text" placeholder="Selecione" />
                     </div>
 
                     <div className="inpute">
@@ -54,16 +76,16 @@ export default function Card() {
 
                 <div className="cont3">
                     <div className="inpute">
-                        <h1> Genero: </h1>
+                        <h1> Gênero: </h1>
 
-                        <input type="text" />
+                        <input type="text" placeholder="Selecione" />
 
                     </div>
 
                     <div className="inpute">
-                        <h1> Email: </h1>
+                        <h1> E-mail: </h1>
 
-                        <input type="text" />
+                        <input type="text" placeholder=" Digite seu email"/>
                     </div>
 
                 </div>
@@ -77,7 +99,7 @@ export default function Card() {
 
                     <div className="inpute">
                         <h1>CPF:</h1>
-                        <input type="text" />
+                        <input type="text" placeholder="___.___.___.__" />
                     </div>
                     <div className="inpute">
                         <h1> RG: </h1>
@@ -87,8 +109,155 @@ export default function Card() {
             
         </div>
 
+
+        <div className="cabecalho">
+            <h1> 2. Informações Financeiras</h1>
+            
+            <button onClick={colocar} className="i">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z"/>
+                </svg>
+             </button>  
+            
+        </div>  
+
+        <div className={`contener ${exibi ? "show" : "hide"}`}>  
+        <div className="cont3">
+                    <div className="inpute">
+                        <h1> Valor Financeiro: </h1>
+
+                        <input type="text"  placeholder="Por sessão"/>
+
+                    </div>
+
+                    <div className="inpute">
+                        <h1> Valor da Sessão: </h1>
+
+                        <input type="text" placeholder="R$: 0,00"/>
+                    </div>
+
+                </div>
+
+        </div>
+        
+        <div className="cabecalho">
+            <h1> 3. Endereço </h1>
+            
+            <button onClick={endereco} className="i">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z"/>
+                </svg>
+             </button>  
+            
+        </div>  
+
+        <div className={`contener ${ visualizar ? "show" : "hide"}`}>
+
+            <div className="cont4">
+
+                <div className="inpute">
+                    <h1> País: </h1>
+                    <input type="text" placeholder='+55' />
+                </div>
+
+                <div className="inpute">
+                    <h1>CEP:</h1>
+                    <input type="text" placeholder="_____-___" />
+                </div>
+                <div className="inpute">
+                    <h1> Cidade: </h1>
+                    <input type="text" />
+                </div>
+            
+                <div className="inpute1">
+                    <h1> Estado: </h1>
+                    <input type="text" />
+                </div>
+            </div>
+
+            <div className="cont3">
+                    <div className="inpute1">
+                        <h1> Endereço : </h1>
+
+                        <input type="text"  />
+
+                    </div>
+
+                    <div className="inpute2">
+                        <h1> Numero: </h1>
+
+                        <input type="text" />
+                    </div>
+
+            </div>
+
+            <div className="cont3">
+                    <div className="inpute">
+                        <h1> Bairro: </h1>
+
+                        <input type="text"  />
+
+                    </div>
+
+                    <div className="inpute">
+                        <h1> Complemento: </h1>
+
+                        <input type="text" />
+                    </div>
+
+                </div>
+            
+        </div>
+
+        <div className="cabecalho">
+            <h1> 4. Responsavel </h1>
+            
+            <button onClick={enderec} className="i">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894z"/>
+                </svg>
+             </button>  
+            
+        </div>  
+
+        <div className={`contener ${ coloca ? "show" : "hide"}`}>  
+
+             <div className="cont1">
+                    <div className="inpute">
+                        <h1>Nome do Responsável : </h1>
+                        <input type="text" placeholder="Digite seu nome"/>
+                    </div>
+                </div>    
+            
+
+            <div className="cont3">
+
+                    <div className="inpute">
+                        <h1> CPF : </h1>
+
+                        <input type="text"  placeholder="___.___.___-__"/>
+
+                    </div>
+            
+                    <div className="inpute">
+                        <h1> Telefone : </h1>
+
+                        <input type="text" placeholder="(__) _____-____"/>
+                    </div>
+            </div>
+        </div>     
+
+        
+        
+
+
     </div>
+
+
+        
     );
+
+    
 }
 
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,15 @@
 import './index.scss';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import Card from '../../components/cards';
+import { Link } from 'react-router-dom';
+
 
 export default function Home() {
 
-   
+    
 
     return(
         <div className="home">
-
+            
             <div className="menu">
 
                 <Link className="picons">
@@ -17,7 +17,7 @@ export default function Home() {
                     <svg id='icons' xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#08B4AC" class="bi bi-people-fill" viewBox="0 0 16 16">
                     <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
                     </svg>
-                    
+    
                     <h1> Cliente </h1>
                 </Link>
 
@@ -60,7 +60,7 @@ export default function Home() {
                     <h1> Config </h1>
                 </div>
             </div>
-
+           
             <div className="people">
 
                 <div className="arrumar">
@@ -75,6 +75,12 @@ export default function Home() {
                 <Card/>
 
             </div>
+
+            
+
+          
+           
+           
            
         </div>
     )
